fd06c35e55cdbc533a4ba3c8387e219e12c05f2d
heyimmatt <mrmcfarland@gmail.com>
2020-08-12 22:11:55 -0700

    Add test file and smoke test for App component

---
+++ snack-or-booze/src/App.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import App from './App';
+
+describe('App Tests', () => {
+
+  it('renders without crashing', () => {
+    render(<App/>);
+  });
+
+})
