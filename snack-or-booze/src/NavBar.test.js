4712070119cf18b77f8fdad74fd547214b1d0605
heyimmatt <mrmcfarland@gmail.com>
2020-08-12 23:40:19 -0700

    Remove unnecessary variable in test

--- snack-or-booze/src/NavBar.test.js
+++ snack-or-booze/src/NavBar.test.js
@@ -5,16 +5,7 @@ import '@testing-library/jest-dom/extend-expect';
 import NavBar from './NavBar';
 
 describe('Menu Tests', () => {
-  const testItems = [
-    {
-      id: 'gt-kombucha',
-      name: 'GT Kombucha',
-      description: 'Sweet, sour, tart, fizzy goodness',
-      recipe: 'Black tea, green tea, assorted fruit juices, and scoby',
-      serve: 'Do not shake. Twist cap. Drink straight from bottle.'
-    }
-  ];
-
+  
   it('renders without crashing', () => {
     render(
       <MemoryRouter>
